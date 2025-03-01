fb8939db0bf091b16e42807d63895b99003f43d4
Vojtěch Jirásek <vojtajiras@gmail.com>
2025-03-01 19:47:13 +0100

    ještě malý upravy, hokus pokus

--- index.php
+++ index.php
@@ -10,117 +10,119 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
-
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">Kino Pyramida</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="#">Program</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Ceník</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <header class="text-center py-5">
-        <h1>Vítejte v našem kině</h1>
-        <p>Podívejte se na nejnovější filmy a užijte si skvělý filmový zážitek!</p>
-    </header>
-
+<nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
-        <div id="filmCarousel" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="card mb-4">
-                                <img src="img/dan.jpg" class="card-img-top" alt="Film 1">
-                                <div class="card-body">
-                                    <h5 class="card-title">Film 1</h5>
-                                    <p class="card-text">Krátký popis filmu.</p>
-                                    <a href="#" class="btn btn-primary">Více informací</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card mb-4">
-                                <img src="img/dan.jpg" class="card-img-top" alt="Film 2">
-                                <div class="card-body">
-                                    <h5 class="card-title">Film 2</h5>
-                                    <p class="card-text">Krátký popis filmu.</p>
-                                    <a href="#" class="btn btn-primary">Více informací</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card mb-4">
-                                <img src="img/dan.jpg" class="card-img-top" alt="Film 3">
-                                <div class="card-body">
-                                    <h5 class="card-title">Film 3</h5>
-                                    <p class="card-text">Krátký popis filmu.</p>
-                                    <a href="#" class="btn btn-primary">Více informací</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="card mb-4">
-                                <img src="img/dan.jpg" class="card-img-top" alt="Film 4">
-                                <div class="card-body">
-                                    <h5 class="card-title">Film 4</h5>
-                                    <p class="card-text">Krátký popis filmu.</p>
-                                    <a href="#" class="btn btn-primary">Více informací</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card mb-4">
-                                <img src="img/dan.jpg" class="card-img-top" alt="Film 5">
-                                <div class="card-body">
-                                    <h5 class="card-title">Film 5</h5>
-                                    <p class="card-text">Krátký popis filmu.</p>
-                                    <a href="#" class="btn btn-primary">Více informací</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card mb-4">
-                                <img src="img/dan.jpg" class="card-img-top" alt="Film 6">
-                                <div class="card-body">
-                                    <h5 class="card-title">Film 6</h5>
-                                    <p class="card-text">Krátký popis filmu.</p>
-                                    <a href="#" class="btn btn-primary">Více informací</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#filmCarousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#filmCarousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+        <a class="navbar-brand" href="#">
+            <img src="icons/pyramida.png" alt="Pyramida" width="30" height="30" class="d-inline-block align-text-top">
+            Kino Pyramida
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="#">Program</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Ceník</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>
+            </ul>
         </div>
     </div>
+</nav>
 
-    <footer class="text-center py-4 mt-5" style="background-color: #1c1c1c;">
-        <p>&copy; 2025 Kino. Všechna práva vyhrazena.</p>
-    </footer>
+<header class="text-center py-5">
+    <h1>Vítejte v našem kině</h1>
+    <p>Podívejte se na nejnovější filmy a užijte si skvělý filmový zážitek!</p>
+</header>
 
-    <script src="js/bootstrap.bundle.min.js"></script>
+<div class="container">
+    <div id="filmCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="img/dan.jpg" class="card-img-top" alt="Film 1">
+                            <div class="card-body">
+                                <h5 class="card-title">Film 1</h5>
+                                <p class="card-text">Krátký popis filmu.</p>
+                                <a href="#" class="btn btn-primary">Více informací</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="img/dan.jpg" class="card-img-top" alt="Film 2">
+                            <div class="card-body">
+                                <h5 class="card-title">Film 2</h5>
+                                <p class="card-text">Krátký popis filmu.</p>
+                                <a href="#" class="btn btn-primary">Více informací</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="img/dan.jpg" class="card-img-top" alt="Film 3">
+                            <div class="card-body">
+                                <h5 class="card-title">Film 3</h5>
+                                <p class="card-text">Krátký popis filmu.</p>
+                                <a href="#" class="btn btn-primary">Více informací</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="img/dan.jpg" class="card-img-top" alt="Film 4">
+                            <div class="card-body">
+                                <h5 class="card-title">Film 4</h5>
+                                <p class="card-text">Krátký popis filmu.</p>
+                                <a href="#" class="btn btn-primary">Více informací</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="img/dan.jpg" class="card-img-top" alt="Film 5">
+                            <div class="card-body">
+                                <h5 class="card-title">Film 5</h5>
+                                <p class="card-text">Krátký popis filmu.</p>
+                                <a href="#" class="btn btn-primary">Více informací</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="img/dan.jpg" class="card-img-top" alt="Film 6">
+                            <div class="card-body">
+                                <h5 class="card-title">Film 6</h5>
+                                <p class="card-text">Krátký popis filmu.</p>
+                                <a href="#" class="btn btn-primary">Více informací</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#filmCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#filmCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+
+<footer class="text-center py-4 mt-5" style="background-color: #1c1c1c;">
+    <p>&copy; 2025 Kino. Všechna práva vyhrazena.</p>
+</footer>
+
+<script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
